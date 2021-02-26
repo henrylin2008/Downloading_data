d99c9e16e610039e4e69ee81f74fb286ec94982f
hlin <henrylin2008@yahoo.com>
2021-02-26 15:07:38 -0800

    adding hover text

--- Json_data/eq_world_map.py
+++ Json_data/eq_world_map.py
@@ -12,14 +12,16 @@ all_eq_dicts = all_eq_data['features']
 # print(len(all_eq_dicts))    # 158
 
 # extracting magnitudes, longitudes, latitudes
-mags, lons, lats = [], [], []   # magnitude
+mags, lons, lats, hover_texts = [], [], [], []   # magnitude
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']      # title will be hover_texts
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 # print(mags[:10])    # first 10 magnitudes: [0.96, 1.2, 4.3, 3.6, 2.1, 4, 1.06, 2.3, 4.9, 1.8]
 # print(lons[:5])     # first 5 longitudes: [-116.7941667, -148.9865, -74.2343, -161.6801, -118.5316667]
@@ -31,6 +33,7 @@ data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': [5*mag for mag in mags],    # marker size
         'color': mags,                      # mags list to determine the color
